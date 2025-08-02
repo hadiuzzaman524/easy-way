5722f1800ab9b59dfe948b49de647cfb82ffa29b
Md Hadiuzzaman <hadiuzzaman.cse.bd@gmail.com>
2025-08-02 09:34:58 -0700

    refactor: change native splash icon

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:easy_way/injector/injector.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class AppBlocObserver extends BlocObserver {
